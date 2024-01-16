afbe73dafb27040fd8fa9b6b11597dde23576af0
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2024-01-16 15:17:14 +0900

    1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}
+
+
+public class Cat : IComparable
+{
+    double temperatureF;
+    public int CompareTo(object? obj)
+    {   
+        if(obj==null) return 1;
+        Cat mycat = obj as Cat;
+        if(mycat!=null)
+        {
+        return this.temperatureF.CompareTo(mycat.temperatureF);
+        }
+        else
+        {
+        throw new NotImplementedException();
+        }
+        
+    }
+}
+public class Dog : IComparer
+{
+    public int Compare(object? x, object? y)
+    {
+        throw new NotImplementedException();
+    }
+}
